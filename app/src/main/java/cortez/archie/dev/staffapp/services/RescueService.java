15e15ec53cf7cc17a9b9f29b00e6192333e641ca
Aqudei <fdeu.aalmine@gmail.com>
2017-10-20 07:55:32 +0800

    ldksldksd

--- app/src/main/java/cortez/archie/dev/staffapp/services/RescueService.java
+++ app/src/main/java/cortez/archie/dev/staffapp/services/RescueService.java
@@ -1,8 +1,12 @@
 package cortez.archie.dev.staffapp.services;
 
 import cortez.archie.dev.staffapp.models.Center;
+import cortez.archie.dev.staffapp.models.CheckIn;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -13,5 +17,8 @@ public interface RescueService {
 
     @GET("centers/{id}/")
     Call<Center> getCenterInfo(@Path("id") String id);
+
+    @POST("people/{id}/check_in/")
+    Call<ResponseBody> uploadOneCheckin(@Path("id") String id, @Body CheckIn checkIn);
 
 }
